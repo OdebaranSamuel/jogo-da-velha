efa3df4a7d1536dd7d4f895d90a4cdab8cb72f9a
Samuel Odebaran <125310099+OdebaranSamuel@users.noreply.github.com>
2023-07-29 22:47:49 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const botao = document.querySelectorAll(".botao");
+const jogador = document.querySelector(".jogador");
+
+const matriz = [];
+let cont = 0;
+
+botao.forEach((elemento) => {
+    elemento.addEventListener("click", () => {
+        return console.log(elemento.innerHTML);
+    });
+});
+
+// function adicionaMatriz() {}
+// for (let linha = 0; linha < 3; linha++) {
+//     for (let coluna = 0; coluna < 3; coluna++) {
+//         console.log(matriz[linha][coluna]);
+//     }
+// }
